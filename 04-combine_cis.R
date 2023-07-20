fba53250d3178bebfd007de9112b93ee77b8f50c
Chris Cox <chriscox@lsu.edu>
2023-07-19 22:26:57 -0500

    color version

--- 04-combine_cis.R
+++ 04-combine_cis.R
@@ -43,17 +43,45 @@ df <- map2(conds$metrics, conds$labels, function(metric, label, x) {
     left_join(y_pred)
 
 
+df$metric <- factor(df$metric, levels = c("indegree", "aspl", "clust"))
 df$value <- factor(df$value, levels = c("ASD", "NA", "diff"))
 df$sig <- sign(df$cil) == sign(df$ciu)
-ggplot(df, aes(x = vocab_size, y = t0, shape = value, fill = sig)) +
-    scale_shape_manual(values = c(21, 22, 23)) +
-    scale_fill_manual(values = c("white", "grey")) +
-    geom_pointrange(aes(ymin = cil, ymax = ciu)) +
-    geom_point(data = df %>% filter(sig)) +
+df$val_sig_bw <- factor(
+    if_else(df$sig, as.character(df$value), paste(as.character(df$value), "ns", sep = "_")),
+    levels = c("ASD_ns", "NA_ns", "diff_ns","ASD", "NA", "diff")
+)
+p_bw <- ggplot(df, aes(x = vocab_size, y = t0, shape = val_sig_bw, fill = val_sig_bw)) +
+    scale_shape_manual(values = c(21, 22, 23, 21, 22, 23)) +
+    scale_fill_manual(values = c("white", "white", "white", "grey", "grey", "grey")) +
+    geom_linerange(aes(ymin = cil, ymax = ciu)) +
+    geom_point() +
     geom_abline(intercept = 0, slope = 0) +
     facet_grid(~metric) +
     xlab("vocabulary size") +
     ylab("RAN-standardized value") +
     theme_bw(base_size = 18)
 
-ggsave("na-asd_netstats_bs10000_conf-bonf_bw.png", width = 12, height = 6, dpi = 300)
+ggsave("na-asd_netstats_bs10000_conf-bonf_bw.png", plot = p_bw, width = 12, height = 6, dpi = 300)
+ggsave("na-asd_netstats_bs10000_conf-bonf_bw.pdf", plot = p_bw, width = 12, height = 6, dpi = 300)
+
+df$val_sig <- factor(
+    if_else(df$sig, as.character(df$value), "ns"),
+    levels = c("ns", "ASD", "NA", "diff")
+)
+df$val_notsig <- factor(
+    if_else(!df$sig, as.character(df$value), "sig"),
+    levels = c("sig", "ASD", "NA", "diff")
+)
+p_col <- ggplot(df, aes(x = vocab_size, y = t0, fill = val_sig, color = val_notsig)) +
+    scale_fill_manual(values = c("white", "#1b9e77", "#d95f02", "#7570b3")) +
+    scale_color_manual(values = c("black", "#1b9e77", "#d95f02", "#7570b3")) +
+    geom_linerange(aes(ymin = cil, ymax = ciu), color = "black") +
+    geom_point(shape = 21, size = 2) +
+    geom_abline(intercept = 0, slope = 0) +
+    facet_grid(~metric) +
+    xlab("vocabulary size") +
+    ylab("RAN-standardized value") +
+    theme_bw(base_size = 18)
+
+ggsave("na-asd_netstats_bs10000_conf-bonf_col.png", plot = p_col, width = 12, height = 6, dpi = 300)
+ggsave("na-asd_netstats_bs10000_conf-bonf_col.pdf", plot = p_col, width = 12, height = 6, dpi = 300)
