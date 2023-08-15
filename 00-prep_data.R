825336a42c99e69f9ecd04ad60cdbafe6a6c00bf
Chris Cox <chriscox@lsu.edu>
2023-08-14 19:17:56 -0500

    Prep data from Stan for pipeline

---
+++ 00-prep_data.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(purrr)
+library(boot)
+library(progressr)
+
+# Data from Stan
+load("data/ASD_NA_metadataNetstats_2023_RAN_14Aug.Rdata")
+
+# Previous input data frame
+d <- readRDS(file = "data/asd-na_netstats-ran.rds")
+
+# Subset, filter to discrete, and compare
+e <- ASD_NA_metadataNetstats_2023_RAN %>%
+    select(
+        subjectkey,
+        interview_age,
+        sex,
+        nproduced = nProduced,
+        group,
+        z_indegree_mean,
+        z_clust,
+        z_dist
+    ) %>%
+    ungroup() %>%
+    distinct()
+
+str(d)
+str(e)
+
+# Save the new base data
+saveRDS(e, file = "data/asd-na_netstats-ran_2023Aug14.rds")
